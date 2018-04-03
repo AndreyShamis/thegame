e47c0a95f43f8741c1502120383151ff564c19dc
Andrey Shamis <lolnik@gmail.com>
2018-04-03 09:41:45 +0300

    composer require twig
    Change-Id: I4d2c224f1002e64d540aa88ee4fb0c18123eade7
    Signed-off-by: Andrey Shamis <lolnik@gmail.com>

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,5 @@ return [
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
