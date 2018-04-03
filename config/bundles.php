cdce446911aa100346e33efbaf6455b3cac569d8
Andrey Shamis <lolnik@gmail.com>
2018-04-03 11:46:29 +0300

    Add web profiler
    Change-Id: I6760022fecba834625f3624d0438a63b4b332177
    Signed-off-by: Andrey Shamis <lolnik@gmail.com>

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,5 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
